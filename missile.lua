989dcc308255124f66ac2d2ec16f2685db691762
theodosispreston-jpg <theodosispreston@gmail.com>
2025-12-05 16:12:50 -0500

    Update missile.lua

--- missile.lua
+++ missile.lua
@@ -1,1 +1,11 @@
-Beep(1)
+
+text TextLabel()
+
+local function TextLabel()
+touchscreen:CreateElement("TextLabel", {
+        Text = "Missile Status: ";
+        TextScaled = true;
+        Size = {X=1, Y=1}; -- Fill the screen
+        Position = {X=0, Y=0};
+    })
+end
