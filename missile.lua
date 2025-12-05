de51239703c48e7cdf4db833b183819d60bd138d
theodosispreston-jpg <theodosispreston@gmail.com>
2025-12-05 16:22:51 -0500

    Update missile.lua

--- missile.lua
+++ missile.lua
@@ -1,2 +1,2 @@
 
-local touchscreen = GetPartFromPort(1, "TouchScreen") TextLabel() local function TextLabel()touchscreen:CreateElement("TextLabel", {        Text = "Missile Status: ";TextScaled = true;        Size = {X=1, Y=1}; -- Fill the screen        Position = {X=0, Y=0};        }) end
+beep(1)
